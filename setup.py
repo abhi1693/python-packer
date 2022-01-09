5a8c2fa7fd20b3d2707ce543164ab5b0ff151031
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2022-01-09 14:33:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*parts):
 
 setup(
 	name='python-packer',
-	version='1.0.5',
+	version='1.0.7',
 	url='https://github.com/abhi1693/python-packer',
 	author='abhi1693',
 	author_email='desk.abhimanyu@gmail.com',
